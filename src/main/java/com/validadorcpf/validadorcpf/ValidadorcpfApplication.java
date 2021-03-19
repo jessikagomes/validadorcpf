4b0180bf47f044deccaa170d4b6f736a7351023a
jessikagomes <jessikagomes@hotmail.com.br>
2021-03-19 18:15:53 -0300

    first commit

---
+++ src/main/java/com/validadorcpf/validadorcpf/ValidadorcpfApplication.java
@@ -0,0 +1,13 @@
+package com.validadorcpf.validadorcpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorcpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidadorcpfApplication.class, args);
+	}
+
+}
